32017cc457bdab30010b9aaf53a628df92dc315e
gfortin13 <xguillaumex@hotmail.com>
2013-11-27 18:46:24 -0500

    added proj

---
+++ application/views/test.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>test</title>
+
+
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to faggots!</h1>
+
+	<div id="body">
+		<p>test</p>
+	</div>
+
+</div>
+
+</body>
+</html>
