e1c8bc5b681021ce81ab6cde266c3dfea0043243
gfortin13 <xguillaumex@hotmail.com>
2013-11-29 19:45:19 -0500

    phil commit
    commited guillaumes work from laptop

---
+++ application/models/test_model.php
@@ -0,0 +1,15 @@
+<?php
+class Test_model extends CI_Model {
+
+	public function __construct()
+	{
+		$this->load->database();
+		$this->load->library('SSH');
+	}
+	
+	public function testQuery(){
+		$query = $this->db->query('SELECT data FROM test');
+		
+		return $query->row_array();
+	}
+}
