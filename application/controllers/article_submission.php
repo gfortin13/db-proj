52cb68f002d795a7063400a5680782c680a8af51
kazo0 <steve.bilogan@gmail.com>
2013-12-02 18:41:24 -0500

    asd

--- application/controllers/article_submission.php
+++ application/controllers/article_submission.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Article_submition extends CI_Controller
+class Article_submission extends CI_Controller
 {
 	public function __construct()
 	{
