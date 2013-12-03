291d04babd7943b1419ce4cc5049554e41cc4dd1
kazo0 <steve.bilogan@gmail.com>
2013-12-02 22:31:00 -0500

    add committee member populated dropdown

--- application/controllers/add_committee_member.php
+++ application/controllers/add_committee_member.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Event_creation extends CI_Controller {
+class Add_committee_member extends CI_Controller {
 
 	public function __construct()
 	{
@@ -17,7 +17,21 @@ class Event_creation extends CI_Controller {
 
 	public function add($eventID){
 
+		$this->load->helper('form');
+		$this->load->library('form_validation');
+
 		$data['users'] = $this->steve_model->getAllUsers();
+		$data['eventID'] = $eventID;
+
+		$this->form_validation->set_rules('users', 'Users', 'trim|required|xss_clean');
+
+
+		if ($this->form_validation->run() === FALSE)
+		{
+			$this->load->view('header', $data);
+			$this->load->view('add_committee_member');
+			$this->load->view('footer');	
+		}
 
 	}
 }
