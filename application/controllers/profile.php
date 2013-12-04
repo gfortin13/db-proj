0bff06a0fb2ec263dbc96837885ad21971eb8eec
tomasvalettini@gmail.com <tomasvalettini@gmail.com>
2013-12-04 15:35:32 -0500

    profile commit!!!

--- application/controllers/profile.php
+++ application/controllers/profile.php
@@ -1,36 +1,97 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Profile extends CI_Controller
-{
-	var $data;
+class Profile extends CI_Controller {
 
 	public function __construct()
 	{
 		parent::__construct();
-
+		$this->load->model('data_model');
+		$this->load->model('readonly_model');
 	}
 
 	public function index()
 	{
-		echo "hello!!!";
-	}
-
-	private function loadHelperModules()
-	{
 		$this->load->helper('form');
 		$this->load->library('form_validation');
+
+		$data['title'] = 'User Profile';
+		$data['page_title'] = 'User Profile';
+
+		$data['user'] = $this->session->userdata('logged_in');
+
+		echo "User array: <br />";
+		print_r($data['user']);
+		echo "<br />";
+
+		$data['user_titles'] = array("Mr." => "Mr.", "Ms." => "Ms.");
+		$data['countries'] = $this->readonly_model->getAllCountries();
+		$data['organizations'] = $this->readonly_model->getAllOrganizations();
+
+		/*echo "<pre>";
+		print_r($data['countries']);
+		echo "</pre>";
+		die;*/
+		$this->form_validation->set_rules('title', 'Title', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('first_name', 'First Name', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('last_name', 'Last Name', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('country', 'Country', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('organization', 'Organization', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('department', 'Department', 'trim|required|xss_clean');
+		$this->form_validation->set_rules('address', 'Address', 'trim|xss_clean');
+		$this->form_validation->set_rules('city', 'City', 'trim|xss_clean');
+		$this->form_validation->set_rules('province', 'Province', 'trim|xss_clean');
+		$this->form_validation->set_rules('postcode', 'Postcode', 'trim|xss_clean');
+		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|xss_clean|callback_email_matches');
+		$this->form_validation->set_rules('conf_email', 'Confirm email', 'trim|required|xss_clean');
+
+		if ($this->form_validation->run() === FALSE)
+		{
+			$this->load->view('header', $data);
+			$this->load->view('profile');
+			$this->load->view('footer');	
+		}
+		else
+		{
+			$data['title'] = 'Registe Complete';
+			$data['page_title'] = 'Register Complete';
+			
+			$data['user']['password'] = $this->_generateRandomString(8);
+			$this->data_model->createUser($data['user']);
+			$newId = $this->data_model->getUserIdByEmail($data['user']['email']);
+			$data['user']['userId'] = $newId['userID'];
+
+			if(false){
+				mail($data['user']['email'], "Your user account", "Your user ID to connect is " . $newId);
+				mail($data['user']['email'], "Your user password", "Your user ID to connect is " . $data['user']['password']);
+				
+			}
+			$this->load->view('header', $data);
+			$this->load->view('register_complete');
+			$this->load->view('footer');
+		}
 	}
 
-	private function setPageTitles()
+	public function email_matches($email)
 	{
-		$this->data['title'] = 'User Profile';
-		$this->data['page_title'] = "User Profile";
+		$confirm_email = $this->input->post('conf_email');
+
+		if($email == $confirm_email)
+		{
+			return TRUE;
+		}
+		else
+		{
+			$this->form_validation->set_message('email_matches', 'Email does not match please verify.');
+		 	return false;
+		}
 	}
 
-	private function show($page = 'profile', $errors = NULL)
-	{
-		$this->load->view('header', $this->data);
-		$this->load->view($page, $errors);
-		$this->load->view('footer');
+	private function _generateRandomString($length = 10) {
+	    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	    $randomString = '';
+	    for ($i = 0; $i < $length; $i++) {
+	        $randomString .= $characters[rand(0, strlen($characters) - 1)];
+   		}
+    	return $randomString;
 	}
 }
--- application/views/profile.php
+++ application/views/profile.php
@@ -1,9 +1,72 @@
-<div>
-    <div style="width: 500px; margin: 0 auto;">
-        <div class="error">
+<div id="content">
+    <div style="width: 700px; margin: 0 auto;">
+    	<div class="error">
             <?php echo validation_errors(); ?>
-        </div>
+    	</div>
 
-        <p>Profile bitch!!</p>
-    </div>
+    	<?php echo form_open('profile') ?>
+    		<p>
+                <label for="title">Title: </label> 
+                <select name="title">
+               		<?php foreach ($user_titles as $key => $user_title) { ?>
+                		<option value="<?php echo $key; ?>" <?php echo set_select('title', $key); ?>><?php echo $user_title; ?></option>
+                	<?php } ?>
+           		</select>
+            </p>
+            <p>
+                <label for="first_name">First Name: </label> 
+                <input type="input" size="30" name="first_name" value="<?php echo set_value('first_name'); ?>" /> <span class="red">*</span>
+            </p>
+            <p>
+                <label for="last_name">Last Name: </label> 
+                <input type="input" size="30" name="last_name" value="<?php echo set_value('last_name'); ?>"/> <span class="red">*</span>
+            </p>
+            <p>
+                <label for="country" >Country: </label> 
+                <select name="country">
+               		<?php foreach ($countries as $key => $country) { ?>
+                		<option value="<?php echo $country['countryid']; ?>" <?php echo set_select('country', $country['countryid']); ?>><?php echo $country['country']; ?></option>
+                	<?php } ?>
+           		</select> <span class="red">*</span>
+            </p>
+            <p>
+                <label for="organization" >Organization: </label> 
+                <select name="organization">
+               		<?php foreach ($organizations as $key => $organization) { ?>
+                		<option value="<?php echo $organization['orgid']; ?>" <?php echo set_select('organization', $organization['orgid']); ?>><?php echo $organization['org_name']; ?></option>
+                	<?php } ?>
+           		</select> <span class="red">*</span>
+            </p>
+            <p>
+                <label for="department" >Department: </label> 
+                <input type="input" size="30" name="department" value="<?php echo set_value('department'); ?>" /> <span class="red">*</span>
+            </p>
+            <p>
+                <label for="address" >Address: </label> 
+                <input type="input" size="30" name="address" value="<?php echo set_value('address'); ?>" />
+            </p>
+            <p>
+                <label for="city" >City: </label> 
+                <input type="input" size="30" name="city" value="<?php echo set_value('city'); ?>" />
+            </p>
+            <p>
+                <label for="province" >Province/State: </label> 
+                <input type="input" size="30" name="province" value="<?php echo set_value('province'); ?>" />
+            </p>
+            <p>
+                <label for="postcode" >Postcode: </label> 
+                <input type="input" size="30" name="postcode" value="<?php echo set_value('postcode'); ?>" />
+            </p>
+            <p>
+                <label for="email" >Email: </label> 
+                <input type="input" size="30" name="email" value="<?php echo set_value('email'); ?>" /> <span class="red">*</span>
+            </p>
+
+            <br><br>
+            
+            <p class="center">
+                <input type="submit" name="submit" value="Update Profile" /> 
+            </p>
+        </form>
+    </div> 
 </div>
