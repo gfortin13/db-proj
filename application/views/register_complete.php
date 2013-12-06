206dfd8ef9716e6cd0c2babba52131c2ca7561e7
tomasvalettini@gmail.com <tomasvalettini@gmail.com>
2013-12-06 13:00:02 -0500

    lsjdfl;jsa

--- application/views/register_complete.php
+++ application/views/register_complete.php
@@ -1,17 +1,22 @@
 <div id="content">
-    <h2>Login within 24h to complete registration.</h2>
-	<?php echo form_open('register') ?>
-       
-        <input id="UID" type="hidden" name="user_id" value="<?php echo $user['userId']; ?>" />
-        <input id="UPW" type="hidden" name="password" value="<?php echo $user['password']; ?>" />
+    <div class="wrap">
+        <center>
 
-    </form>
-    <script>
-        $(function() {
-            var userID = $("#UID").val();
-            var userPW = $("#UPW").val();
-            alert("Your user ID is " + userID);
-            alert("Your password is " + userPW); 
-        } );
-    </script>
+        <h2>Login within 24h to complete registration.</h2>
+    	<?php echo form_open('register') ?>
+           
+            <input id="UID" type="hidden" name="user_id" value="<?php echo $user['email']; ?>" />
+            <input id="UPW" type="hidden" name="password" value="<?php echo $user['password']; ?>" />
+
+        </form>
+        <script>
+            $(function() {
+                var userID = $("#UID").val();
+                var userPW = $("#UPW").val();
+                alert("Your user ID is " + userID);
+                alert("Your password is " + userPW); 
+            } );
+        </script>
+        </center>
+    </div>
 </div>
